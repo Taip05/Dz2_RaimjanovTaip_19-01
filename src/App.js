0b2365505f7ef768844495a2296b2d98f756e175
Taip05 <taipraimjanov@gmail.com>
2022-12-14 17:25:57 +0300

    hw2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyInfo from "./components/MyInfo";
+
+function App() {
+    return (
+        <div>
+            <MyInfo/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo(props) {
+    return (
+        <div>
+            <h1>Taip</h1>
+            <p>Мне 17, учусь на 4 мясеце</p>
+            <ol>
+                <li>Бишкек</li>
+                <li>Ош</li>
+                <li>Алматы</li>
+            </ol>
+        </div>
+    );
+}
+
+export default MyInfo;
